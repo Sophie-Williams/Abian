3944c4ab1c0742fed350d21d183c9d57d144f06d
Ethan Henderson <ethan@zbee.me>
2015-02-12 23:35:24 -0700

    More attribution

--- footer.php
+++ footer.php
@@ -5,7 +5,10 @@
   <footer class="footer">
     <div class="container">
       <p class="pull-right" id="btt"><a href="#">Back to top</a></p>
-      <p class="text-muted">Abian created by Zbee (with help from Twitch viewers).</p>
+      <p class="text-muted">
+        Abian created by <a href="https://github.com/Zbee">Zbee</a> (with  a lot
+        of help from Twitch viewers)
+      </p>
       <p class="text-muted">Server time is <?=date("Y-m-d\TH:i", time())?> (America/Denver - MST)</p>
     </div>
   </footer>
